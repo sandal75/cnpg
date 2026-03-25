29ede752fbca09cea9eaa2704a5e00455bcf07ff
sandal75 <anders.sandal@gmail.com>
2026-03-25 10:44:00 +0100

    Create baseinstall.bash

---
+++ baseinstall.bash
@@ -0,0 +1,36 @@
+## INSTALL ##
+
+# install KIND
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
+
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# install kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.35/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.35/rpm/repodata/repomd.xml.key
+EOF
+
+sudo yum install -y kubectl
+
+
+# install helm
+sudo dnf install -y helm
+
+
+# install docker
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+
+
+# create cluster
+kind create cluster --name pg
+kubectl cluster-info
+~
